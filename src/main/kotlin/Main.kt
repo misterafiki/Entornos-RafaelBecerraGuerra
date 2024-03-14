8276badadd0acc9fc6e16e9ed1dcee5a2df13193
Rafael Becerra Guerra <rafikibece9@gmail.com>
2024-03-14 12:35:51 +0100

    refac_3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,16 +6,8 @@ fun main(args: Array<String>) {
     var sumaFilas = Array(m.size){0}
     var sumaColum = Array(m[0].size){0}
 
-    for (f in m.indices){
-        for (c in m[f].indices){
-            print("${m[f][c]} ")
-        }
-        println()
-    }
-    var f: Int = 0
-    var c: Int = 0
-
-    sumarFilas(f, m, c, sumaFilas)
+    mostar(m)
+    sumarFilas(m, sumaFilas)
 
     sumaColumnas(m,sumaColum)
     println("La suma de las filas es")
@@ -25,21 +17,12 @@ fun main(args: Array<String>) {
 
 }
 
-private fun sumarFilas(
-    f: Int,
-    m: Array<Array<Int>>,
-    c: Int,
-    sumaFilas: Array<Int>
-) {
-    var f1 = f
-    var c1 = c
-    while (f1 < m.size) {
-        while (c1 < m[0].size) {
-            sumaFilas[f1] += m[f1][c1]
-            c1++
+private fun mostar(m: Array<Array<Int>>) {
+    for (f in m.indices) {
+        for (c in m[f].indices) {
+            print("${m[f][c]} ")
         }
-        c1 = 0
-        f1++
+        println()
     }
 }
 
@@ -54,6 +37,20 @@ fun sumaColumnas(m:Array<Array<Int>>, sumaColumnas:Array<Int>){
         }
         f = 0
         c++
+    }
+}
+
+private fun sumarFilas(m: Array<Array<Int>>, sumaFilas: Array<Int>) {
+    var f: Int = 0
+    var c: Int = 0
+
+    while (f < m.size) {
+        while (c < m[0].size) {
+            sumaFilas[f] += m[f][c]
+            c++
+        }
+        c = 0
+        f++
     }
 }
 
